3fb3bd267e6e7eae6deabfea0665a4d3b9d57e7c
Denys Malevych <73424177+Wample@users.noreply.github.com>
2023-05-14 02:52:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace laba6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File.ReadAllLines("input.txt").Select(int.Parse);
+
+            
+            var sortedNumbers = numbers.OrderBy(x => x);
+
+            
+            File.WriteAllLines("output.txt", sortedNumbers.Select(x => x.ToString()));
+        }
+    }
+}
